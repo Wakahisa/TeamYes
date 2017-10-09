634648bc9cff52deb9bea59c26b80d2534b72fe8
jcampbe8 <jcampbe8@alaska.edu>
2017-10-08 21:03:33 -0800

    succesfully added database, next is more models and some data

---
+++ startAppA1/startAppA1/Models/Admin/AdminTableModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace startAppA1.Models.Admin
+{
+    public class AdminTableModel
+    {
+        [Key]
+        public int TableID { get; set; }
+    }
+}
